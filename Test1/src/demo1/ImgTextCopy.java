0d6d378f728b76b9011067fd2560dd16d8bc82c7
naipings <115548001+naipings@users.noreply.github.com>
2023-10-14 18:46:35 +0800

    Add files via upload

--- Test1/src/demo1/ImgTextCopy.java
+++ Test1/src/demo1/ImgTextCopy.java
@@ -7,6 +7,8 @@ import static src.demo1.FileCopyer.arrayList;
 
 public class ImgTextCopy {
 
+	private static String[] lineStrings = new String[5];
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		FileCopyer copyer = new FileCopyer();
@@ -20,33 +22,36 @@ public class ImgTextCopy {
 			throw new RuntimeException(e);
 		}
 
+
 		try {
 			copyer.testReadFile(targetPath);
-			for ( int i=1; i< arrayList.size()+1; i++ ) {
-				String targetPath0 = String.format("C:\\images\\www.swu.edu.cn\\xywh\\zhanbanzongban\\0%d.jpg", i);
-				copyer.copyFromWeb(arrayList.get(i-1), targetPath0);
+			for ( int i=0; i< arrayList.size(); i++ ) {
+				String rs = copyer.testReadFileOrder(targetPath, i);
+				System.out.println("rs="+rs);
+				String targetPath0 = String.format("C:\\images\\www.swu.edu.cn\\xywh\\%s\\01.jpg", rs);
+				lineStrings[i] = targetPath0;
+				copyer.copyFromWeb(arrayList.get(i), targetPath0);
 			}
-
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 
+
+
 		FileCompare fileCompare = new FileCompare();
-		String s = "C:\\images\\www.swu.edu.cn\\xywh\\zhanbanzongban\\";
-		fileCompare.orderByLength(s);
+		fileCompare.orderByLength2(lineStrings);
 		String[] strings = new String[arrayList1.size()];
 		try {
 			String targetPath1 = "C:\\images\\images-sorted.txt";
 			for ( int i=0; i< arrayList1.size(); i++ ) {
 				//System.out.println(s+arrayList1.get(i));
-				strings[i] = s+arrayList1.get(i);
+				strings[i] = arrayList1.get(i);
 			}
 			copyer.testWriteFile(strings, targetPath1);
 
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
-
 
 	}
 
